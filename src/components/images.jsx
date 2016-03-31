3fae32112891938f5cc65e3813975fe40d7351fc
Andrew Timney <andrew.timney@gmail.com>
2016-03-31 18:06:11 +0100

    Flex images

--- src/components/images.jsx
+++ src/components/images.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { ImageFiles } from '../services/image-files'
+import _ from 'lodash'
 
 export class Images extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { new: [], old: [] };
+  }
+  componentWillMount(){
+    var imgFiles = new ImageFiles();
+    var images = imgFiles.getNewAndOld();
+    this.setState(images);
+  }
   render(){
-    return <h1>Wit</h1>
+
+    var imgs = [];
+    var just = _.take(this.state.new, 100);
+    just.forEach(function(e, i){
+      imgs.push(<div key={i}>
+          <img src={e.path} width="250" />
+        </div>);
+    });
+
+    return <div>
+      <div style={{display: "flex"}}>
+        {imgs}
+      </div>
+        <div>{this.state.new.length}</div>
+        <div>Images here</div>
+      </div>
   }
 }
